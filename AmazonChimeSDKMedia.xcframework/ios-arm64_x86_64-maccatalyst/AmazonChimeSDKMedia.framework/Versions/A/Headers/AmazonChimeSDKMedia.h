d01243a07ad2940116338de11b4d0566e4f6e193
Matt Pennig <mpennig@slack-corp.com>
2022-06-03 09:54:03 -0500

    add xcframeworks

---
+++ AmazonChimeSDKMedia.xcframework/ios-arm64_x86_64-maccatalyst/AmazonChimeSDKMedia.framework/Versions/A/Headers/AmazonChimeSDKMedia.h
@@ -0,0 +1,21 @@
+//
+//  AmazonChimeSDKMedia.h
+//  AmazonChimeSDKMedia
+//
+//  Created by Matt Pennig on 6/1/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AmazonChimeSDKMedia.
+FOUNDATION_EXPORT double AmazonChimeSDKMediaVersionNumber;
+
+//! Project version string for AmazonChimeSDKMedia.
+FOUNDATION_EXPORT const unsigned char AmazonChimeSDKMediaVersionString[];
+
+#import "AudioClientSDK.h"
+#import "VideoClientSDK.h"
+#import "VideoClientMetric.h"
+#import "VideoRendererDelegate.h"
+#import "audio_client_enum.h"
+#import "video_client_enum.h"
